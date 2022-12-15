23aabefd44892494d425ac51586b40efcf9cedfc
Wenbo Li <liwenbo4936@gmail.com>
2022-12-15 13:20:06 -0600

    webpack-heroku

--- server/routes/htmlRoutes.js
+++ server/routes/htmlRoutes.js
@@ -2,7 +2,7 @@ const path= require('path');
 
 module.exports=function(app){
     app.get('/',function(req,res){
-        res.sendFile(path.join(__dirname, '../../client/indes.html'));
+        res.sendFile(path.join(__dirname, '../../client/dist/indes.html'));
     });
 };
 
